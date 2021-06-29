11349185c9d865c6ab061a4944c1c38b2f8bf9ab
pisarev <2368404+pisarev@users.noreply.github.com>
2021-06-29 12:12:55 +0300

    Update 04_task_if_else.py

--- Module2/practice/if_else/04_task_if_else.py
+++ Module2/practice/if_else/04_task_if_else.py
@@ -4,4 +4,10 @@
 # а если делится на 3 и на 5 - вывести “Foobar”.
 # Для всех остальных случаев не выводить ничего.
 
-# TODO: your code here
+value = int(input())
+if value % 3 == 0 and value % 5 == 0:
+    print("Foobar")
+elif value % 3 == 0:
+    print("Foo")
+elif value % 5 == 0:
+    print("Bar")
