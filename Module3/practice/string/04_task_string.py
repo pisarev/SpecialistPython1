61a25384ea4feb02cfd07bb6489820f26d12ba2f
pisarev <2368404+pisarev@users.noreply.github.com>
2021-06-29 17:12:36 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -4,4 +4,14 @@
 text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
-# TODO: your code here
+i = 0
+j = 0
+count = 0
+while i < len(text):
+    if text[i] == " ":
+        if i > 0 and i - j - 1 > 5:
+            count += 1
+        j = i
+    i += 1
+
+print(count)
