06f3a4abc3f607af394c03fc416492de6a746fe4
pisarev <2368404+pisarev@users.noreply.github.com>
2021-06-30 00:01:32 +0300

    Update 04_hw.py

--- Module2/home_work/04_hw.py
+++ Module2/home_work/04_hw.py
@@ -14,3 +14,21 @@
 #    #
 #    #
 ######
+
+import math
+length = int(input())
+matrix = [["" for j in range(length)] for i in range(length)]
+
+x = 0
+y = 0
+while x < length:
+    y = 0
+    while y < length:
+        if x == 0 or x == length - 1 or y == 0 or y == length - 1:
+            matrix[x][y] = "#"
+        else:
+            matrix[x][y] = " "
+        y += 1
+    x += 1
+for char in matrix:
+    print(char)
