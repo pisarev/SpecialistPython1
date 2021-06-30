fd212141a93c60874db50a621548c50e82cb3168
pisarev <2368404+pisarev@users.noreply.github.com>
2021-06-30 14:56:07 +0300

    Update 09_task_tuple.py

--- Module3/practice/list_and_tuple/09_task_tuple.py
+++ Module3/practice/list_and_tuple/09_task_tuple.py
@@ -1,4 +1,28 @@
 # Дано 3 кортежа заполненные произвольными целыми числами.
 # Найдите количество элементов, которые встречаются во всех трех кортежах.
+a = []
+b = []
+c = []
+import random
+i = 0
+while i < random.randint(10, 100):
+    a.append(random.randint(0, 0xffffff))
+    i += 1
 
-# TODO: your code here
+i = 0
+while i < random.randint(10, 100):
+    b.append(random.randint(0, 0xffffff))
+    i += 1
+
+i = 0
+while i < random.randint(10, 100):
+    c.append(random.randint(0, 0xffffff))
+    i += 1
+
+a = tuple(a)
+b = tuple(b)
+c = tuple(c)
+
+print(len(a))
+print(len(b))
+print(len(c))
