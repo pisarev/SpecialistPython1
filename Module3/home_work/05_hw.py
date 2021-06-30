a44a3f9498b8341efa54f126399ff5cd1bf4277d
pisarev <2368404+pisarev@users.noreply.github.com>
2021-06-30 23:43:08 +0300

    Update 05_hw.py

--- Module3/home_work/05_hw.py
+++ Module3/home_work/05_hw.py
@@ -2,5 +2,13 @@
 # Найдите самое длинное имя, если таких имен несколько - выведи любое их них
 
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
-
+max_length = 0
+name_index = -1
+for i, name in enumerate(names):
+    j = len(name)
+    if max_length < j:
+        max_length = j        
+        name_index = i
+if name_index != -1:
+    print(names[name_index])
 # TODO: your code here
