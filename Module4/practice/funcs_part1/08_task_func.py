22e606a2b0b94413eacd83211067e77d8db3081f
pisarev <2368404+pisarev@users.noreply.github.com>
2021-07-01 14:43:01 +0300

    Update 08_task_func.py

--- Module4/practice/funcs_part1/08_task_func.py
+++ Module4/practice/funcs_part1/08_task_func.py
@@ -1,1 +1,9 @@
 # Напишите функцию находящую n-ое число Фибоначчи.
+def fibonacci_number(number_index, index = 0, prior_value_1 = 0, prior_value_2 = 0):
+    if index == 1:
+        prior_value_1 = 1
+    if index == number_index:
+        return prior_value_1
+    return fibonacci_number(number_index, index + 1, prior_value_1 + prior_value_2, prior_value_1)
+
+print(fibonacci_number(int(input())))
